8bae27648e7788e0a2e3d525df55202c4224fa1a
afransayyed10 <afransayyed45@gmail.com>
2023-05-09 13:05:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { extend } from '@syncfusion/ej2-base';
+import { KanbanComponent, ColumnsDirective, ColumnDirective } from "@syncfusion/ej2-react-kanban";
+import { kanbanData } from './datasource';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const data = [
+    {
+      "Id": "1",
+      "Tittle": "Task 1",
+      "Status": "Open",
+      "Summary": 'Analyze the new requirements gathered from the customer.',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "2",
+      "Tittle": "Task 2",
+      "Status": "Open",
+      "Summary": 'POD scrum.',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "3",
+      "Tittle": "Task 1",
+      "Status": "InProgress",
+      "Summary": 'lorem ipsum',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "4",
+      "Tittle": "Task 2",
+      "Status": "InProgress",
+      "Summary": 'Siddhesh Shirodkar.',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "5",
+      "Tittle": "Task 1",
+      "Status": "InProgress",
+      "Summary": 'SJ Innovation',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "6",
+      "Tittle": "Task 1",
+      "Status": "Testing",
+      "Summary": 'Amol Bhandari',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "7",
+      "Tittle": "Task 1",
+      "Status": "Testing",
+      "Summary": 'Sankalp',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+    {
+      "Id": "8",
+      "Tittle": "Task 1",
+      "Status": "Testing",
+      "Summary": 'Amey Phadte',
+      "Type": 'Story',
+      "Priority": 'Normal',
+      "Estimate": "2",
+      "Assignee": "Nancy Davloio",
+    },
+  ];
+
+
+
+    return(<KanbanComponent id="kanban" keyField="Status" dataSource={data} cardSettings={{ contentField: "Summary", headerField: "Id" }}>
+        <ColumnsDirective>
+          <ColumnDirective headerText="To Do" keyField="Open"/>
+          <ColumnDirective headerText="In Progress" keyField="InProgress"/>
+          <ColumnDirective headerText="Testing" keyField="Testing"/>
+          <ColumnDirective headerText="Done" keyField="Close"/>
+        </ColumnsDirective>
+    </KanbanComponent>);
+    
+}
 export default App;
